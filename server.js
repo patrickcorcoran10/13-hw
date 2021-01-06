5947e18e478a46c5d8abf9435e54f22b852d9624
Patty C <patrickcorcoran10@gmail.com>
2021-01-06 12:42:35 -0600

    commit with front end attached

--- server.js
+++ server.js
@@ -12,6 +12,13 @@ app.use(express.json());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+app.get("/", function (req, res) {
+
+
+    res.render("index");
+});
+
+
 app.listen(PORT, function () {
     // Log (server-side) when our server has started
     console.log("Server listening on: http://localhost:" + PORT);
