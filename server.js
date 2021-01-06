182323d7434227b0d125f180dc15f44d2f7dbac4
Patty C <patrickcorcoran10@gmail.com>
2021-01-06 00:42:57 -0600

    second commit

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
 const orm = require("./config/orm.js");
+const express = require("express")
+const exphbs = require("express-handlebars")
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.listen(PORT, function () {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+});
